a04df0fa2efbe2e79e1ea80870a88af79206aeb8
nuhuxi <mark.keckeis@gmail.com>
2015-05-19 15:30:25 -0700

    Forgot to put the () after .defer

--- app/data/weather.service.js
+++ app/data/weather.service.js
@@ -4,33 +4,27 @@
 /**
  * Created by mark07 on 5/19/15.
  */
-(function(){
+(function () {
   angular.module("app.data")
-    .factory('weatherService',  function($http, $q){
+    .factory("weatherService", function ($http, $q) {
       return {
         find: findByLocation
-      };
+      }
 
       function findByLocation(location) {
-
         var url = "http://api.openweathermap.org/data/2.5/find?q=" + location;
 
-        var defer = $q.defer;
+        var defer = $q.defer();
 
         $http.get(url)
-          .success (function(response){
-          defer.resolve(response);
-        })
-          .error(function(err) {
-            defer.reject(err)
+          .success(function (response) {
+            defer.resolve(response);
+          })
+          .error(function (err) {
+            defer.reject(err);
           })
 
         return defer.promise;
       }
     });
-
-
-
-
-
-})();
+}());
