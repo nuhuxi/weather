5d3841196f1d1e6ab28455342412d72c536002e1
nuhuxi <mark.keckeis@gmail.com>
2015-05-19 16:03:37 -0700

    Before Lecture 81 - everything working great

--- app/search/search.controller.js
+++ app/search/search.controller.js
@@ -1,13 +1,15 @@
 (function(){
   angular.module("app.search")
     .controller("search", function($scope, weatherService){
+      $scope.cities = null;
+
       $scope.$on("search", search);
 
       function search(evt, data){
         weatherService.find(data.str)
           .then(
           function(response){
-           console.log(response);
+           $scope.cities = response.list;
           },
           function(err) {
            console.log("Error finding cities: ", err)
